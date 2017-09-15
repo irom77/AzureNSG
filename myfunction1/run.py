5dd1ae48f331c8b7245cb6f09da2dee2d529255d
ptglynn <pglynn@paloaltonetworks.com>
2017-09-15 08:16:45 -0500

    Update run.py
    Initial import of updated code

--- myfunction1/run.py
+++ myfunction1/run.py
@@ -5,14 +5,19 @@
     http://MediaRealm.com.au/
 """
 
-import sys, os
+import sys, os, httplib, json, urllib, ast
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'lib')))
-
-import json
 from AzureHTTPHelper import HTTPHelper
 
 # This is a little class used to abstract away some basic HTTP functionality
 http = HTTPHelper()
+
+#c = httplib.HTTPSConnection("ccc.de")
+#c.request("GET", "/")
+#response = c.getresponse()
+#print response.status, response.reason
+#data = response.read()
+#print data
 
 # All these print statements get sent to the Azure Functions live log
 print "--- GET ---"
@@ -32,22 +37,93 @@ for x in http.env:
     print x
 print
 
+ARMResource = 'https://management.core.windows.net/'
 
-# All data to be returned to the client gets put into this dict
-returnData = {
-    #HTTP Status Code:
-    "status": 200,
-    
-    #Response Body:
-    "body": "<h1>Azure Works :)</h1>",
-    
-    # Send any number of HTTP headers
-    "headers": {
-        "Content-Type": "text/html",
-        "X-Awesome-Header": "YesItIs"
-    }
-}
+ClientSecret = str(http.post['ClientSecret'])[2:-4]
 
-# Output the response to the client
-output = open(os.environ['res'], 'w')
-output.write(json.dumps(returnData))
+ResourceGroupName = str(http.post['ResourceGroupName'])[2:-4]
+
+ClientID = str(http.post['ClientID'])[2:-4]
+
+TenantID = str(http.post['TenantID'])[2:-4]
+
+Attacker = str(http.post['Attacker'])[2:-4]
+
+NetworkSecurityGroupName = str(http.post['NetworkSecurityGroupName'])[2:-4]
+
+SubscriptionID = str(http.post['SubscriptionID'])[2:-4]
+
+Region = str(http.post['Region'])[2:-4]
+
+Host = 'login.windows.net'
+
+URL = '/' + TenantID + '/oauth2/token/'
+
+Headers = { 'User-Agent': 'python', 'Content-Type': 'application/x-www-form-urlencoded', }
+
+tempstr = { 'resource' : ARMResource, 'client_id' : ClientID, 'grant_type' : 'client_credentials', 'client_secret' : ClientSecret }
+
+QueryStr = urllib.urlencode(tempstr)
+
+conn = httplib.HTTPSConnection(Host)
+
+conn.request("POST", URL, QueryStr, Headers)
+
+response = conn.getresponse()
+
+data = response.read()
+
+datadict = ast.literal_eval(data)
+
+AccessToken = datadict['access_token']
+
+Host = 'management.azure.com'
+
+URL = '/subscriptions/' + SubscriptionID + '/resourceGroups/' + ResourceGroupName + '/providers/Microsoft.Network/networkSecurityGroups/' + NetworkSecurityGroupName + '?api-version=2017-08-01'
+
+Headers = { 'Content-Type': 'application/json', 'Authorization': 'bearer ' + AccessToken }
+
+Body = json.dumps({
+  "location": Region,
+  "properties": {
+    "defaultSecurityRules": [],
+    "securityRules": [
+      {
+        "name": Attacker,
+        "properties": {
+          "protocol": "*",
+          "sourceAddressPrefix": Attacker,
+          "destinationAddressPrefix": "*",
+          "access": "Deny",
+          "destinationPortRange": "*",
+          "sourcePortRange": "*",
+          "priority": 100,
+          "direction": "Outbound",
+          "provisioningState": "Updating"
+        }
+      }
+    ]
+  }
+})
+
+conn = httplib.HTTPSConnection(Host)
+
+conn.request("PUT", URL, Body, Headers)
+
+response = conn.getresponse()
+
+data = response.read()
+
+print Host
+print ARMResource
+print ClientSecret
+print ResourceGroupName
+print ClientID
+print TenantID
+print Attacker
+print NetworkSecurityGroupName
+print SubscriptionID
+print QueryStr
+print 'Response: ', response.status, response.reason
+print 'Data:'
+print data
